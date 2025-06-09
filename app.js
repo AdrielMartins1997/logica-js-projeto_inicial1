6e617d75f6a3c2c0927c1fe1f9fea156105a5264
Adriel Martins ;) <adrielmartinspessoal@gmail.com>
2025-06-09 17:15:44 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 2;
+let chute;
+let tentativas = 1;
+
+// Enquanto o chute não for igual ao nmr secreto
+while (chute != numeroSecreto) {
+    chute = Number(prompt('Escolha um número entre 1 e 10'));
+// Se o chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`isso ai descobriu o número ${numeroSecreto} com ${tentativas} tentativas.`);     
+    } else if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+    } else {
+            alert(`O número secreto é maior que ${chute}`);    
+        }  
+        tentativas++;
+
+
+    }
+
+// testando o meu aprendizado com git e githubbbbb'
